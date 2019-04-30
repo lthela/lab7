bf3d68c7d58726ea17a4bb523087115f1837b9ff
laishwaryat <46529112+laishwaryat@users.noreply.github.com>
2019-04-30 10:34:06 +0530

    Update open.c

--- open.c
+++ open.c
@@ -26,7 +26,6 @@
 #include "vnode.h"
 #include "vmnt.h"
 #include "path.h"
-#include "vnode.h"
 
 static char mode_map[] = {R_BIT, W_BIT, R_BIT|W_BIT, 0};
 
@@ -109,17 +108,10 @@ int common_open(char path[PATH_MAX], int oflags, mode_t omode)
 
   /* If O_CREATE is set, try to make the file. */
   if (oflags & O_CREAT) {
-	 
-	  if(vp->v_dev==897){
-	         omode= I_IMMEDIATE | (omode & ALLPERMS & fp->fp_umask);
-		  vp=new_node(&resolve, oflags, omode);
-		  r=err_code;
-		 
-	  }
-	  else{
-        omode = I_REGULAR | (omode & ALLPERMS & fp->fp_umask);
+	  
+        omode = I_IMMEDIATE | (omode & ALLPERMS & fp->fp_umask);
 	vp = new_node(&resolve, oflags, omode);
-	r = err_code;}
+	r = err_code;
 	if (r == OK) exist = FALSE;	/* We just created the file */
 	else if (r != EEXIST) {		/* other error */
 		if (vp) unlock_vnode(vp);
